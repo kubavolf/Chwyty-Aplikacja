53fd0ab25038917fd3acd921fc48dbe6066e826f
lgorro <115094304+lgorro@users.noreply.github.com>
2022-12-19 23:04:31 +0100

    Update main.py

--- App/main.py
+++ App/main.py
@@ -1,3 +1,5 @@
+import ast
+
 from window import Window
 import sys
 from PyQt6.QtWidgets import QApplication
@@ -5,62 +7,28 @@ from PyQt6.QtWidgets import QApplication
 from Chord import Chord, Tuner, Metronome
 
 
+
 app = QApplication(sys.argv)
 
 window = Window()
 
+akord_list = []
 
-akord_c = Chord(variant="C_chords", x_cor=60, y_cor=300, wg=150, hg=50,
-                image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
+with open("akord_base.txt") as f:
+    data = f.read()
 
-akord_d = Chord(variant="D_chords", x_cor=240, y_cor=300, wg=150, hg=50,
-                image_7="border-image:url(D-7.png)", image_dur="border-image:url(D-dur.png)", image_mol="border-image:url(D-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
+akord_dict = ast.literal_eval(data)
 
-akord_e = Chord(variant="E_chords", x_cor=420, y_cor=300, wg=150, hg=50,
-                image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_f = Chord(variant="F_chords", x_cor=600, y_cor=300, wg=150, hg=50,
-                image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_g = Chord(variant="G_chords", x_cor=150, y_cor=400, wg=150, hg=50,
-                image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_a = Chord(variant="A_chords", x_cor=330, y_cor=400, wg=150, hg=50,
-                image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_b = Chord(variant="B_chords", x_cor=510, y_cor=400, wg=150, hg=50,
-                image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_cis = Chord(variant="Cis_chords", x_cor=60, y_cor=500, wg=150, hg=50,
-                  image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_dis = Chord(variant="Dis_chords", x_cor=240, y_cor=500, wg=150, hg=50,
-                  image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_fis = Chord(variant="Fis_chords", x_cor=420, y_cor=500, wg=150, hg=50,
-                  image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_gis = Chord(variant="Gis_chords", x_cor=600, y_cor=500, wg=150, hg=50,
-                  image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
-
-akord_h = Chord(variant="H_chords", x_cor=330, y_cor=200, wg=150, hg=50,
-                image_7="border-image:url(C-7.png)", image_dur="border-image:url(C-dur.png)", image_mol="border-image:url(C-mol.png)",
-                sound_7="Sounds/rocket.wav", sound_dur="Sounds/rocket.wav", sound_mol="Sounds/rocket.wav")
+for akord in akord_dict:
+    akord = Chord(variant=akord_dict[akord]["variant"], x_cor=akord_dict[akord]["x_cor"], y_cor=akord_dict[akord]["y_cor"], wg=akord_dict[akord]["wg"], hg=akord_dict[akord]["hg"],
+                  image_7=akord_dict[akord]["image_7"], image_dur=akord_dict[akord]["image_dur"], image_mol=akord_dict[akord]["image_mol"],
+                  sound_7=akord_dict[akord]["sound_7"], sound_dur=akord_dict[akord]["sound_dur"], sound_mol=akord_dict[akord]["sound_mol"])
+    akord_list.append(akord)
 
 
-
-
+for akord in akord_list:
+    akord.create_new_window()
+    akord.create_button(window)
 
 tuner = Tuner(variant="Tuner", x_cor=600, y_cor=100, wg=150, hg=150,
                 image_e6="border-image:url(C-7.png)", image_a="border-image:url(C-dur.png)", image_d="border-image:url(C-mol.png)",
@@ -68,14 +36,11 @@ tuner = Tuner(variant="Tuner", x_cor=600, y_cor=100, wg=150, hg=150,
                 sound_e6="Sounds/E6.wav", sound_a="Sounds/A.wav", sound_d="Sounds/D.wav", sound_g="Sounds/G.wav",
                 sound_b="Sounds/B.wav", sound_e1="Sounds/E1.wav")
 
-metronome = Metronome(variant="Metronome", x_cor=100, y_cor=100, wg=150, hg=150, image_7="border-image:url(C-7.png)", sound_7="Sounds/metron_tick.wav", BPM=0.5)
+metronome = Metronome(variant="Metronome", x_cor=60, y_cor=100, wg=150, hg=150, image_7="border-image:url(C-7.png)", sound_7="Sounds/metron_tick.wav", BPM=0.5)
 
-akord_list = [akord_c, akord_d, akord_e, akord_f, akord_g, akord_a, akord_b, akord_cis, akord_dis, akord_fis, akord_gis, akord_h, tuner]
 
-for akord in akord_list:
-    akord.create_button(window)
-    akord.create_new_window()
-
+tuner.create_button(window)
+tuner.create_new_window()
 metronome.create_button(window)
 metronome.create_new_window()
 
