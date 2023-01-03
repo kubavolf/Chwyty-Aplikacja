836ee78694873c664ce1b7a0707077a046ff8676
kubavolf <115459342+kubavolf@users.noreply.github.com>
2023-01-03 11:36:59 +0100

    Najnowsze zmiany

---
+++ App/akord_base.py
@@ -0,0 +1,49 @@
+config = {
+            "akord_c": {"variant": "C_chords", "x_cor": 60, "y_cor": 300, "wg":150, "hg": 50, "image_7": "border-image:url(Chords/C-7.png)",
+                        "image_dur": "border-image:url(Chords/C-dur.png)", "image_mol": "border-image:url(Chords/C-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_d": {"variant": "D_chords", "x_cor": 240, "y_cor": 300, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/D-7.png)",
+                        "image_dur": "border-image:url(Chords/D-dur.png)", "image_mol": "border-image:url(Chords/D-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_e": {"variant": "E_chords", "x_cor": 420, "y_cor": 300, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/E-7.png)",
+                        "image_dur": "border-image:url(Chords/E-dur.png)", "image_mol": "border-image:url(Chords/E-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": " Sounds/rocket.wav"},
+
+            "akord_f": {"variant": "F_chords", "x_cor": 600, "y_cor": 300, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/F-7.png)",
+                        "image_dur": "border-image:url(Chords/F-dur.png)", "image_mol": "border-image:url(Chords/F-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_g": {"variant": "G_chords", "x_cor": 60, "y_cor": 400, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/G-7.png)",
+                        "image_dur": "border-image:url(Chords/G-dur.png)", "image_mol": "border-image:url(Chords/G-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_a": {"variant": "A_chords", "x_cor": 240, "y_cor": 400, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/A-7.png)",
+                        "image_dur": "border-image:url(Chords/A-dur.png)", "image_mol": "border-image:url(Chords/A-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_b": {"variant": "B_chords", "x_cor": 420, "y_cor": 400, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/B-7.png)",
+                        "image_dur": "border-image:url(Chords/B-dur.png)", "image_mol": "border-image:url(Chords/B-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_cis": {"variant": "Cis_chords", "x_cor": 600, "y_cor": 400, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/Cis-7.png)",
+                        "image_dur": "border-image:url(Chords/Cis-dur.png)", "image_mol": "border-image:url(Chords/Cis-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_dis": {"variant": "Dis_chords", "x_cor": 60, "y_cor": 500, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/Dis-7.png)",
+                        "image_dur":"border-image:url(Chords/Dis-dur.png)", "image_mol": "border-image:url(Chords/Dis-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol":"Sounds/rocket.wav"},
+
+            "akord_fis": {"variant": "Fis_chords", "x_cor": 240, "y_cor": 500, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/Fis-7.png)",
+                        "image_dur": "border-image:url(Chords/Fis-dur.png)", "image_mol": "border-image:url(Chords/Fis-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+
+            "akord_gis": {"variant": "Gis_chords", "x_cor": 420, "y_cor": 500, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/Gis-7.png)",
+                        "image_dur":"border-image:url(Chords/Gis-dur.png)", "image_mol": "border-image:url(Chords/Gis-mol.png)",
+                        "sound_7":"Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol":"Sounds/rocket.wav"},
+
+            "akord_h": {"variant": "H_chords", "x_cor": 600, "y_cor": 500, "wg": 150, "hg": 50, "image_7": "border-image:url(Chords/H-7.png)",
+                        "image_dur": "border-image:url(Chords/H-dur.png)", "image_mol": "border-image:url(Chords/H-mol.png)",
+                        "sound_7": "Sounds/rocket.wav", "sound_dur": "Sounds/rocket.wav", "sound_mol": "Sounds/rocket.wav"},
+}
